3f636f8d2ab3b6cb2030fba0bcfb3e272d77587c
blasterbug <tontonposso@gmail.com>
2015-02-24 14:14:09 +0000

    insert

---
+++ SQL/insert_metiers.sql
@@ -0,0 +1,9 @@
+create sequence seqmetiers;
+insert into metiersvalues (seqmetiers.currentval, 'redacteur en chef', 3000);
+insert into metiers values (seqmetiers.nextval, 'redacteur', 1900);
+insert into metiers values (seqmetiers.nextval, 'pigiste', 1500);
+insert into metiers values (seqmetiers.nextval, 'secretaire de redaction', 1800);
+insert into metiers values (seqmetiers.nextval, 'photographe', 1800);
+insert into metiers values (seqmetiers.nextval, 'illustrateur', 1800);
+insert into metiers values (seqmetiers.nextval, 'maquettiste', 1800);
+insert into metiers values (seqmetiers.nextval, 'expert', 1800);
---
+++ SQL/insert_personnes.sql
@@ -0,0 +1,182 @@
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Dupont',
+  'Marie-Christine',
+  0498657421,
+  1
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Chorizo',
+  'Julia',
+  0489657896,
+  1
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Martins',
+  'Jacques',
+  0489657256,
+  2
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Rousseau',
+  'Paul',
+  0489698432,
+  4
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Miron',
+  'Orane',
+  0421857433,
+  3
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Zahaf',
+  'Abed',
+  0432659854,
+  6
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Vioko',
+  'Nikita',
+  0447913548,
+  5
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Lim',
+  'Richard',
+  0213493281,
+  7
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Beauchesne',
+  'Anton',
+  0325898617,
+  4
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Huot',
+  'Annette',
+  0432659854,
+  1
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Marleau',
+  'Charlène',
+  0489541934,
+  3
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'De la Ronde',
+  'Tempeste',
+  0432659854,
+  4
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Perrault',
+  'Édith',
+  0432659854,
+  1
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Parizeau',
+  'Benjamin',
+  0432659854,
+  1
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Clavette',
+  'Octave',
+  0193117445,
+  4
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'René',
+  'Hélène',
+  0501658465,
+  6
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Rivard',
+  'Martin',
+  0265679855,
+  6
+);
+
+INSERT INTO personnes(idPers, nomPers, prenom, numTel,  idMetier)
+VALUES 
+(
+  substr( sys_guid(), 8, 8 ),
+  'Thériault',
+  'Faustin',
+  0194199551,
+  3
+);
+
+select * from personnes;
