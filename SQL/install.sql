8ef333e2faddd641ac79e0f97a8492b32a8ac9a4
Thomas Minier <tminier01@gmail.com>
2015-03-04 13:13:57 +0000

    ajout d'un script générant install.sql

--- SQL/install.sql
+++ SQL/install.sql
@@ -1,40 +1,39 @@
 -- Script installant le projet sur une base Oracle
-
 spool logs_install_wookie_hebdo.lst
 prompt Création de la base de données
 
 -- Création des tables
 @create_table.sql
 
--- Création des procédures
+-- Création des tables
 @procedures/prd_insert_personne.sql
 
 -- Création des vues
-@vues/personnes_metiers.sql
-@vues/vue_Experts.sql
+@vues/vue_RedacsChef.sql
+@vues/vue_Pigistes.sql
+@vues/vue_SecrRedac.sql
+@vues/vue_Photographes.sql
 @vues/vue_Illustrateurs.sql
 @vues/vue_Maquettistes.sql
-@vues/vue_Photographes.sql
-@vues/vue_Pigistes.sql
-@vues/vue_RedacsChef.sql
+@vues/vue_Experts.sql
 @vues/vue_Redacteurs.sql
-@vues/vue_SecrRedac.sql
+@vues/personnes_metiers.sql
 
 -- Création des triggers
 @triggers/trg_check_insert_update_dateRedac.sql
 @triggers/trg_insert_update_dateParu.sql
 @triggers/trg_instead_insert_experts.sql
-@triggers/trg_instead_insert_illustr.sql
-@triggers/trg_instead_insert_maquet.sql
-@triggers/trg_instead_insert_photogr.sql
-@triggers/trg_instead_insert_pigistes.sql
 @triggers/trg_instead_insert_redacsChef.sql
 @triggers/trg_instead_insert_redacs.sql
+@triggers/trg_instead_insert_pigistes.sql
 @triggers/trg_instead_insert_secrRedac.sql
+@triggers/trg_instead_insert_photogr.sql
+@triggers/trg_instead_insert_illustr.sql
+@triggers/trg_instead_insert_maquet.sql
 
 -- Insertion des données
-@inserts/insert_metiers.sql
 @inserts/insert_personnes.sql
+@inserts/insert_metiers.sql
 @inserts/insert_seqcontenu.sql
 @inserts/insert_typejournal.sql
 
---
+++ make_install_script.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Script générant le fichier d'installation de la base : install.sql
+
+import os
+
+# chemins vers les différents dossiers
+path_to_sql = "SQL/"
+size_path_to_sql = len(path_to_sql)
+path_to_inserts = path_to_sql + "inserts/"
+path_to_procedures = path_to_sql + "procedures/"
+path_to_triggers = path_to_sql + "triggers/"
+path_to_vues = path_to_sql + "vues/"
+path_to_file = path_to_sql + "install.sql"
+
+# Fonction utilitaire
+def return_files_import(directory):
+	res = ""
+	for dirname, dirnames, filenames in os.walk(directory):
+		for filename in filenames:
+			res += "@" + os.path.join(dirname[size_path_to_sql:], filename) + "\n"
+
+	return res
+
+with open(path_to_file, 'w') as file:
+
+	# entête du fichier
+	file.write("-- Script installant le projet sur une base Oracle\n")
+	file.write("spool logs_install_wookie_hebdo.lst\n")
+	file.write("prompt Création de la base de données\n")
+
+	file.write("\n")
+
+	# création des tables
+	file.write("-- Création des tables\n")
+	file.write("@create_table.sql\n")
+
+	file.write("\n")
+
+	# création des procédures
+	file.write("-- Création des tables\n")
+	file.write(return_files_import(path_to_procedures))
+
+	file.write("\n")
+
+	# création des vues
+	file.write("-- Création des vues\n")
+	file.write(return_files_import(path_to_vues))
+
+	file.write("\n")
+
+	# création des triggers
+	file.write("-- Création des triggers\n")
+	file.write(return_files_import(path_to_triggers))
+
+	file.write("\n")
+
+	# insertion des données
+	file.write("-- Insertion des données\n")
+	file.write(return_files_import(path_to_inserts))
+
+	file.write("\n")
+
+	# fin du fichier
+	file.write("spool off")
