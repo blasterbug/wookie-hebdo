407ee8b598b8d303bec288a2c53de9d2709e27c6
Thomas Minier <tminier01@gmail.com>
2015-03-18 14:05:07 +0000

    trigger avec un nom trop long :/

--- SQL/triggers/trg_insert_update_dateParu.sql
+++ SQL/triggers/trg_insert_update_dateParu.sql
@@ -1,6 +1,6 @@
 -- Trigger gérant l'insertion de dateParu dans Numeros
 
-CREATE OR REPLACE TRIGGER trg_aft_ins_upd_dateParu_numeros
+CREATE OR REPLACE TRIGGER trg_aft_ins_upd_dateParu
 	AFTER INSERT OR UPDATE ON Numeros
 	FOR EACH ROW
 DECLARE
